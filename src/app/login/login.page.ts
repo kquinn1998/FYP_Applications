5d10e0dd2d8ab3f805a4837254e412ec5af16fed
kquinn1998 <kevquinn321@gmail.com>
2020-01-19 18:18:28 +0000

    initial

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  email: string = "";
+  password: string = "";
+
+  constructor(public afAuth: AngularFireAuth) { }
+
+  ngOnInit() {
+  }
+
+  async login(){
+    const { email, password } = this;
+    try {
+      const res = this.afAuth.auth.signInWithEmailAndPassword(email, password);
+      //const res = this.afAuth.auth.signInWithCredential
+    } catch(err){
+      console.dir(err);
+    }
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    apiKey: "AIzaSyD0tYR6ljXeAPSyGqLG3j1e5EsLRVIWrhM",
+    authDomain: "revolutefitness-a92df.firebaseapp.com",
+    databaseURL: "https://revolutefitness-a92df.firebaseio.com",
+    projectId: "revolutefitness-a92df",
+    storageBucket: "revolutefitness-a92df.appspot.com",
+    messagingSenderId: "831478978518",
+    appId: "1:831478978518:web:f11db4dd0220b6cd173677",
+    measurementId: "G-P8L28N5N1K"
+  }
 };
 
 /*
