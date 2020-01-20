f3be0661a6be24a4015794bd5ba8d68ba33c7b1a
kquinn1998 <kevquinn321@gmail.com>
2020-01-20 21:34:12 +0000

    Starting authguard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IonicStorageModule } from '@ionic/storage';
+
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
@@ -18,7 +20,8 @@ import { environment } from 'src/environments/environment';
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
             AngularFireModule.initializeApp(environment.config),
-            AngularFireAuthModule],
+            AngularFireAuthModule,
+            IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
