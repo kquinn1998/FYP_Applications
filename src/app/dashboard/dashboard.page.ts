bcc3ecda0c24619213300062a378d317f5b2e2ea
kquinn1998 <kevquinn321@gmail.com>
2020-02-10 21:55:20 +0000

    login logout page linking no auth set

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { AuthService } from '../login/login.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  isLoading = false;
+  isLogin = true;
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
+    alert(this.authService.userIsAuthenticated);
+  }
+
+  logout() {
+    this.isLoading = true;
+    this.authService.logout();
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: 'Logging out...' })
+      .then(loadingEl => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/login');
+        }, 1500);
+      });
   }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { AuthService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -12,14 +14,31 @@ export class LoginPage implements OnInit {
 
   email: '';
   password: '';
+  isLoading = false;
+  isLogin = true;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
+    alert(this.authService.userIsAuthenticated);
   }
 
   login(form: NgForm) {
-
+    this.isLoading = true;
+    this.authService.login();
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: 'Logging in...' })
+      .then(loadingEl => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/dashboard');
+        }, 1500);
+      });
   }
 
 }
