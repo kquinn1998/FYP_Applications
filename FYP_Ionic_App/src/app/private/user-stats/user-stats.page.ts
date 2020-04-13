b3b6f6ce9643b4318b87927ad9b9b284a01d23e8
kquinn1998 <kevquinn321@gmail.com>
2020-04-13 17:05:44 +0100

    Further updates on graphs

--- FYP_Ionic_App/src/app/private/user-stats/user-stats.page.ts
+++ FYP_Ionic_App/src/app/private/user-stats/user-stats.page.ts
@@ -13,27 +13,58 @@ import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
 })
 export class UserStatsPage implements OnInit {
 
+  //Normal Page Variables
   private workoutSub: Subscription;
-
   workouts: RecordedWorkout[] = [ ];
-
   isLoading = false;
 
-  chartData: ChartDataSets[] = [{data: [], label: 'Workout Total Weight'}];
-
-  chartLabels: Label[];
-
-  chartOptions = {
+  //Chart1Variables
+  chartData1: ChartDataSets[] = [{data: [], label: 'Total Weight'}];
+  chartLabels1: Label[];
+  chartOptions1 = {
     responsive: true,
     maintainAspectRatio: true,
+    aspectRatio: 1.5,
     scales: {
       xAxes: [{
-        type: 'time',
-        time: {
-          unit: 'month',
-          displayFormats: {
-            day: 'MMM YY'
-          }
+        ticks: {
+          autoSkip: false,
+          maxRotation: 90,
+          minRotation: 90
+        }
+      }]
+    },
+    title: {
+      display: true,
+      text: 'Total Weight'
+    },
+    elements: {
+      line: {
+        tension: 0
+      }
+    }
+  };
+  chartColors1: Color[] = [
+    {
+      borderColor: '#000000',
+      backgroundColor: '#4287f5'
+    }
+  ];
+  chartType1 = 'line';
+
+  //Chart2 Variables
+  chartData2: ChartDataSets[] = [{data: [], label: 'Total Weight'}];
+  chartLabels2: Label[];
+  chartOptions2 = {
+    responsive: true,
+    maintainAspectRatio: true,
+    aspectRatio: 1.5,
+    scales: {
+      xAxes: [{
+        ticks: {
+          autoSkip: false,
+          maxRotation: 90,
+          minRotation: 90
         }
       }]
     },
@@ -47,18 +78,13 @@ export class UserStatsPage implements OnInit {
       }
     }
   };
-
-  chartColors: Color[] = [
+  chartColors2: Color[] = [
     {
       borderColor: '#000000',
       backgroundColor: '#4287f5'
     }
   ];
-
-  chartType = 'line';
-  showLegend = false;
-
-  workoutsText = '';
+  chartType2 = 'line';
 
   constructor(private workoutServ: WorkoutService) { }
 
@@ -80,19 +106,22 @@ export class UserStatsPage implements OnInit {
 
   setData() {
 
-    this.chartData[0].data = [];
-    this.chartLabels = [];
+    this.chartData1[0].data = [];
+    this.chartLabels1 = [];
+    let i: number = this.workouts.length;
 
     for(let entry of this.workouts){
       //let date: Date = new Date(entry.date);
-      //this.chartLabels.push(date.getDate().toString() + ', ' + date.getMonth().toString() + '\n' + entry.title.toString());
-      this.chartLabels.push(entry.date.toString());
+      //this.chartLabels.push(date.getMonth().toString() + ' ' + date.getDate().toString());
+      //console.log(date.getMonth().toString() + ' ' + date.getDate().toString())
+      //this.chartLabels.push(entry.date.toString());
+      this.chartLabels1.push(i.toString() + ' ' + entry.title);
+      i = i - 1;
       let totalWeight = 0;
       for(let weight of entry.weights){
         totalWeight = totalWeight + Number(weight);
       }
-      console.log(totalWeight);
-      this.chartData[0].data.push(totalWeight);
+      this.chartData1[0].data.push(totalWeight);
     }
   }
 
